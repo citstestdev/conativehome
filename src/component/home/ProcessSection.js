03065428226ebd9447325cb07a8a166d343934f0
root <root@20201005>
2022-03-04 17:56:46 +0530

    04 mar

--- src/component/home/ProcessSection.js
+++ src/component/home/ProcessSection.js
@@ -1,67 +1,66 @@
-import React, { useEffect, useState }  from 'react';
-import { backendurl } from '../constants';
+import React, { useEffect, useState } from "react";
+import { backendurl } from "../constants";
 
-function ProcessSection()
-{
-
-
-  const [data, setData] = useState([])
-  const [item, setItem] = useState([])
- 
-  useEffect(() => {
-    fetch(backendurl+"process-show")
-    .then(response => response.json())
-    .then(data => setData(data))
-  },[])
+function ProcessSection() {
+  const [data, setData] = useState([]);
+  const [item, setItem] = useState([]);
 
   useEffect(() => {
-    fetch(backendurl+"processplan-show")
-    .then(res => res.json())
-    .then(item => setItem(item))
-  },[])
+    fetch(backendurl + "process-show")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
 
- // console.log("FDA",item);
+  useEffect(() => {
+    fetch(backendurl + "processplan-show")
+      .then((res) => res.json())
+      .then((item) => setItem(item));
+  }, []);
 
-    return(
-        <div>
-        <section className="our-process-wr">
+  // console.log("FDA",item);
+
+  return (
+    <div>
+      <section className="our-process-wr">
         <div className="center-wrapper">
           <div className="features-widget clearfix">
-              <div className="features-head-blk left wow slideInLeft">
-                <h3>
-                  <span>{data.title}</span>
-                  {data.name}
-                </h3>
-              </div>
-              <div className="features-description-blk right wow" data-splitting>
-                <p>
-                 {data.description}
-                </p>
-              </div>
-          </div>
-            <div className="process-pager-blk">
-              <ul className="process-pager">
-              {item.map((post,index)  => (
-
-                <li key={post._id}><a href="{void(0)}" data-slide-index={index}>{post.name}<span className="dot"></span></a></li>
-                ))}
-              </ul>
+            <div className="features-head-blk left wow slideInLeft">
+              <h3>
+                <span>{data.title}</span>
+                {data.name}
+              </h3>
             </div>
+            <div className="features-description-blk right wow" data-splitting>
+              <p>{data.description}</p>
+            </div>
+          </div>
+          <div className="process-pager-blk">
+            <ul className="process-pager">
+              {item.map((post, index) => (
+                <li key={post._id}>
+                  <a href="{void(0)}" data-slide-index={index}>
+                    {post.name}
+                    <span className="dot"></span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-          <div className="process-slider-area">
-            <div className="process-slider">
-              {item.map((post,index) => (
+        <div className="process-slider-area">
+          <div className="process-slider">
+            {item.map((post, index) => (
               <div className="process-slide" key={post._id}>
                 <figure>
-                  <img src={backendurl+post.image} alt="Conative Process" />
+                  <img src={post.image} alt="Conative Process" />
                 </figure>
               </div>
-              ))}
-            </div>
+            ))}
           </div>
-  </section>
-     </div>
-      )
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default ProcessSection;
